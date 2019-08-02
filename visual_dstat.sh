c96d0e3389fb55df1fd0073c1757af16b3369889
bigdatalyn <88322511@qq.com>
2019-08-02 13:00:56 +0800

    upload visual dstat Ver1.0

---
+++ visual_dstat.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+# Launch webserver and visualize dstat measurement in a browser
+# Create dstat file using this format
+#  $ dstat --time -v --net --output [DSTAT_output_CSV] [DELAY_SEC]
+#  Example:
+#  $ dstat --time -v --net --output dstat.csv 1 10
+#  $ ./visual_dstat.sh dstat.csv
+# If called with no arguments, will launch dstat measurement
+#  $ ./visual_dstat.sh
+
+CSV_FN=$1
+HTML_DIR=html.$(date +"%Y%m%d-%H%M%S")
+mkdir -p $HTML_DIR
+cp -r js $HTML_DIR/.
+cp style.css $HTML_DIR/.
+echo Working directory is $HTML_DIR
+
+kill_server() {
+	echo 
+  [ $DSTAT_PID ] && echo "Stopping dstat " && kill $DSTAT_PID > /dev/null
+  echo "Stopping web server"
+	kill $SRV_PID 2> /dev/null
+}
+trap 'kill_server' SIGTERM SIGINT # Kill when webserver is killed
+
+if [ $# -lt 1 ]
+then
+  # Launch dstat measurement
+  echo Starting dstat for 5 minutes using this command:
+  echo "dstat --time -v --net --output dstat.csv 1 300"
+  dstat --time -v --net --output $HTML_DIR/dstat.csv 1 300 > /dev/null &
+  DSTAT_PID=$!
+  ARG="and dstat measurement"
+  cat index.html | sed "s/refresh_page = false/refresh_page = true/" | \
+     sed "s/Visualize dstat/Visualize dstat on $(hostname)/" \
+     > $HTML_DIR/index.html
+else
+  echo Visualizing file $1
+  cp $1 $HTML_DIR/.
+  cat index.html | sed "s/dstat.csv/$1/g" > $HTML_DIR/index.html
+fi
+  
+cd $HTML_DIR
+
+echo Starting web server--point your browser to http://localhost:2222
+echo 
+python -m SimpleHTTPServer 2222 2> http_log&
+SRV_PID=$!
+sleep 0.5
+read -p "Press return to stop web server $ARG"
+kill_server
